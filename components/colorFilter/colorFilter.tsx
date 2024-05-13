c7f5aca15b36407c124feaa0316a256bbaf325a6
Senouci Moulay <mosenouci@airfrance.fr>
2024-05-13 18:21:07 +0200

    colorFilter Components OK with right color

--- components/colorFilter/colorFilter.tsx
+++ components/colorFilter/colorFilter.tsx
@@ -3,60 +3,33 @@ import { useState } from 'react';
 export const ColorFilter = () => {
     const [selectedColor, setSelectedColor] = useState<string | null>(null);
 
+    const colors = ['blue', 'green', 'yellow', 'orange', 'red', 'purple', 'white', 'black'];
+
     const handleColorClick = (color: string) => {
         setSelectedColor(selectedColor === color ? null : color);
     };
 
     return (
         <div className={'grid grid-cols-8 fixed top-3/4 left-4'}>
-            <span
-                className={`w-16 h-3 bg-blue-600 rounded-l transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'blue' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px]'
-                }`}
-                onClick={() => handleColorClick('blue')}
-            />
-            <span
-                className={`w-16 h-3 bg-green-600 transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'green' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px] '
-                }`}
-                onClick={() => handleColorClick('green')}
-            />
-            <span
-                className={`w-16 h-3 bg-yellow-600 transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'yellow' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px] '
-                }`}
-                onClick={() => handleColorClick('yellow')}
-            />
-            <span
-                className={`w-16 h-3 bg-orange-600 transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'orange' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px] '
-                }`}
-                onClick={() => handleColorClick('orange')}
-            />
-            <span
-                className={`w-16 h-3 bg-red-600 transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'red' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px] '
-                }`}
-                onClick={() => handleColorClick('red')}
-            />
-            <span
-                className={`w-16 h-3 bg-purple-600 transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'purple' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px] '
-                }`}
-                onClick={() => handleColorClick('purple')}
-            />
-            <span
-                className={`w-16 h-3 bg-white transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'white' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px] '
-                }`}
-                onClick={() => handleColorClick('white')}
-            />
-            <span
-                className={`w-16 h-3 bg-black border border-white rounded-r transition-transform duration-300 ease-in-out ${
-                    selectedColor === 'black' ? 'transform translate-y-[-20px] rounded-md scale-110' : 'hover:translate-y-[-2px]'
-                }`}
-                onClick={() => handleColorClick('black')}
-            />
+            {colors.map((color, index, array) => (
+                <span
+                    key={color}
+                    className={`w-16 h-3.5 ${
+                        color === 'white' || color === 'black' ? `bg-${color} border border-white` : `bg-${color}-600`
+                    } transition-all duration-500 ease-in-out ${
+                        selectedColor === color ? 'transform translate-y-[-20px] rounded-md scale-110' :
+                            'hover:translate-y-[-2px]'
+                    } ${
+                        (selectedColor === array[index - 1] && index > 0) ? 'rounded-l-md' : ''
+                    } ${
+                        (selectedColor === array[index + 1] && index < array.length - 1) ? 'rounded-r-md' : ''
+                    }`}
+                    style={{
+                        backgroundColor: color,
+                    }}
+                    onClick={() => handleColorClick(color)}
+                />
+            ))}
         </div>
     );
 };
