730de921deaf0e192ee1e7c4ae3c17cc48fdb01d
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2024-05-14 17:38:54 +0200

    feat: Add search functionality to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -162,7 +162,9 @@ export default function Home({ movies }: InferGetStaticPropsType<typeof getStati
                         </div>
                         <MoviePopable movie={selectedMovie ?? undefined} close={() => setSelectedMovie(null)} />
 
-
+                        <Search className="fixed top-4 right-4 z-20 text-white-500 hover:text-gray-700 font-bold cursor-pointer"
+                            size={40}
+                            onClick={() => setSearchModalOpen(true)} />
 
 
                         <SearchModal
